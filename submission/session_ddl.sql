4abcebfea56200b71686799e67a48d1e04b137ac
mposada97 <85133902+mposada97@users.noreply.github.com>
2024-06-09 19:25:02 -0500

    Update session_ddl.sql
    added partition and comments

--- submission/session_ddl.sql
+++ submission/session_ddl.sql
@@ -1,15 +1,16 @@
 CREATE OR REPLACE TABLE mposada.dataexpert_sessions (
-    session_id VARCHAR,
-    user_id VARCHAR,
-    session_start TIMESTAMP,
-    session_end TIMESTAMP,
-    event_count INTEGER,
-    session_date DATE,
-    city VARCHAR,
-    state VARCHAR,
-    country VARCHAR,
-    operating_system VARCHAR,
-    browser VARCHAR,
-    is_logged_in BOOLEAN,
+    session_id VARCHAR, -- Unique identifier for the session
+    user_id VARCHAR, -- Unique identifier for the user
+    session_start TIMESTAMP, -- Start time of the session
+    session_end TIMESTAMP, -- End time of the session
+    event_count INTEGER, -- Number of events in the session
+    session_date DATE, -- Date of the session
+    city VARCHAR, -- City from which the session originated
+    state VARCHAR, -- State from which the session originated
+    country VARCHAR, -- Country from which the session originated
+    operating_system VARCHAR, -- Operating system used during the session
+    browser VARCHAR, -- Browser used during the session
+    is_logged_in BOOLEAN, -- Whether the user was logged in
     PRIMARY KEY (session_id)
 )
+PARTITION BY DATE(session_date)
