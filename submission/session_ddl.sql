45343a713b4a4e15813956d0e7ed0b6cca6ce8c7
Aleem Rahil <aleem.rahil@outlook.com>
2024-06-11 16:37:31 -0400

    Update session_ddl.sql

---
+++ submission/session_ddl.sql
@@ -0,0 +1,16 @@
+-- Table to store user session data
+CREATE OR REPLACE TABLE ALEEMRAHIL.SESSIONS_LOG(
+    session_id VARCHAR PRIMARY KEY, -- Unique identifier for each session
+    user_id VARCHAR,               -- Unique identifier for the user
+    session_start TIMESTAMP,       -- Start of the session
+    session_end TIMESTAMP,         -- End of the session
+    event_count INTEGER,               -- Number of events in the session
+    session_date DATE,             -- Date of the beginning of the session
+    city VARCHAR,                   -- City associated with the session
+    country VARCHAR,                -- Country associated with the session
+    state VARCHAR,                  -- State associated with the session
+    operating_system VARCHAR,       -- Operating system associated with the session
+    browser VARCHAR,                -- Browser associated with the session
+    is_logged_in BOOLEAN           -- Whether the session is for logged in or logged out users
+)
+PARTITION BY DATE(session_id)
