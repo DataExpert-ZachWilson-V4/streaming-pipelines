ba1b55188f262679174ff61d1418ab742b678591
Hilary Sarneski-Hayes <hilary@Hilarys-MacBook-Air.local>
2024-06-11 17:49:46 -0600

    successful run

---
+++ submission/session_ddl.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS sarneski44638.spark_sessionization (
+    session_id BIGINT,
+    user_id BIGINT,
+    ip STRING,
+    session_start TIMESTAMP,
+    session_end TIMESTAMP,
+    event_count BIGINT,
+    date DATE,
+    city STRING,
+    state STRING,
+    country STRING,
+    os STRING,
+    browser STRING,
+    is_logged_in BOOLEAN
+) USING ICEBERG
+PARTITION BY
+    date;
