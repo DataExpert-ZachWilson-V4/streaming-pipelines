4462bd767e476d586de0141f3fad74ae8f288be9
mposada97 <85133902+mposada97@users.noreply.github.com>
2024-06-11 07:29:53 -0500

    Update session_ddl.sql

--- submission/session_ddl.sql
+++ submission/session_ddl.sql
@@ -1,16 +1,17 @@
 CREATE OR REPLACE TABLE mposada.dataexpert_sessions (
-    session_id VARCHAR, -- Unique identifier for the session
-    user_id VARCHAR, -- Unique identifier for the user
-    session_start TIMESTAMP, -- Start time of the session
-    session_end TIMESTAMP, -- End time of the session
-    event_count INTEGER, -- Number of events in the session
-    session_date DATE, -- Date of the session
-    city VARCHAR, -- City from which the session originated
-    state VARCHAR, -- State from which the session originated
-    country VARCHAR, -- Country from which the session originated
-    operating_system VARCHAR, -- Operating system used during the session
-    browser VARCHAR, -- Browser used during the session
-    is_logged_in BOOLEAN, -- Whether the user was logged in
-    PRIMARY KEY (session_id)
+    host STRING,
+    session_id STRING,
+    user_id BIGINT,
+    session_start TIMESTAMP,
+    session_end TIMESTAMP,
+    session_date DATE,
+    event_count BIGINT,
+    country STRING,
+    state STRING,
+    city STRING,
+    browser_family STRING,
+    device_family STRING,
+    is_logged_in BOOLEAN,
 )
-PARTITION BY session_date
+USING ICEBERG
+PARTITIONED BY session_date
