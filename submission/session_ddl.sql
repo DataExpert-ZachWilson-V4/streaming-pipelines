0455158266d5e88d1e9e09179c8f79d9760d1e89
shashankkongara <121494824+shashankkongara@users.noreply.github.com>
2024-06-12 19:40:05 -0400

    Update session_ddl.sql

--- submission/session_ddl.sql
+++ submission/session_ddl.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE TABLE shashankkongara.kafka_streaming_user_sessions_homework (
     session_id INT,  -- Unique identifier for each session, hashed from user_id, ip, and session start time
     session_start TIMESTAMP(6) WITH TIME ZONE,  -- Start time of the session
     session_end TIMESTAMP(6) WITH TIME ZONE,  -- End time of the session
-    total_events BIGINT,  -- Total number of events that occurred during the session
+    event_count BIGINT,  -- Total number of events that occurred during the session
     start_date DATE,  -- Date when the session started
     country_code VARCHAR,  -- Country associated with the session, based on IP geolocation
     state_name VARCHAR,  -- State associated with the session, based on IP geolocation
