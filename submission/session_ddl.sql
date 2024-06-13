8b3acf5ef7f5dffa66e6c33b516d170d515c2e86
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-06-12 21:16:36 -0700

    Update session_ddl.sql

---
+++ submission/session_ddl.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE TABLE deeptianievarghese22866.session_job_example (
+  user_id VARCHAR,
+  ip VARCHAR,
+  session_id VARCHAR,
+  session_start TIMESTAMP,
+  session_end TIMESTAMP,
+  event_count BIGINT,
+  session_start_date TIMESTAMP,
+  country VARCHAR,
+  state VARCHAR,
+  city VARCHAR,
+  os VARCHAR,
+  browser VARCHAR,
+  logged_in_user_status VARCHAR
+)
