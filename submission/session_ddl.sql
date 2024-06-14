3aaf711fbfc3a5369d11976c083c792740aed72f
Jesse Charbneau <jessecharbneau@gmail.com>
2024-06-14 19:54:28 -0400

    updating per suggestions

--- submission/session_ddl.sql
+++ submission/session_ddl.sql
@@ -3,8 +3,7 @@
 --
 -- Write a DDL query (session_ddl.sql) that creates a table that tracks Data Expert sessions.
 
-
--- Create or replace the  table to track DataExpert.io sessions
+-- Create or replace the table to track DataExpert.io sessions
 CREATE OR REPLACE TABLE jlcharbneau.dataexpert_sessions (
     -- Unique identifier for each session
     session_id VARCHAR,
@@ -19,7 +18,7 @@ CREATE OR REPLACE TABLE jlcharbneau.dataexpert_sessions (
     event_count BIGINT,
 
     -- The date when the session started
-    session_start_date DATE,
+    session_date DATE,
 
     -- City associated with the session
     city VARCHAR,
@@ -31,15 +30,20 @@ CREATE OR REPLACE TABLE jlcharbneau.dataexpert_sessions (
     country VARCHAR,
 
     -- Operating system used during the session
-    os VARCHAR,
+    device_family VARCHAR,
 
     -- Browser used during the session
-    browser VARCHAR,
+    browser_family VARCHAR,
 
     -- Session state indicating whether the user is logged in or out
-    logged_in_or_out VARCHAR
-)
-WITH (
+    is_logged_in BOOLEAN,
+
+    -- Unique identifier for the user
+    user_id VARCHAR,
+
+    -- Host associated with the session
+    host VARCHAR
+) WITH (
     -- Parquet format
     format = 'PARQUET',
     -- Declares partitioning of the table by session_start_date for optimized queries
