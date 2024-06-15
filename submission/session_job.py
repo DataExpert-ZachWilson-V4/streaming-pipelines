d56884fa11670eae92beca8e67ad1d5bef8c061f
AliaHa3 <eng.alia.hamwi@gmail.com>
2024-06-15 07:18:49 +0300

    final formatiing

--- submission/session_job.py
+++ submission/session_job.py
@@ -3,21 +3,37 @@ import sys
 import requests
 import json
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lit, window,session_window, from_json, udf, sha2,when,concat_ws,date_format
-from pyspark.sql.types import StringType, IntegerType, TimestampType, StructType, StructField, MapType
+from pyspark.sql.functions import (
+    col,
+    lit,
+    window,
+    session_window,
+    from_json,
+    udf,
+    sha2,
+    when,
+    concat_ws,
+    date_format,
+)
+from pyspark.sql.types import (
+    StringType,
+    IntegerType,
+    TimestampType,
+    StructType,
+    StructField,
+    MapType,
+)
 from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
 # TODO PUT YOUR API KEY HERE
-GEOCODING_API_KEY = 'D9B3C9F0029B15297D5432BEB3786D5F'
+GEOCODING_API_KEY = "D9B3C9F0029B15297D5432BEB3786D5F"
+
 
 def geocode_ip_address(ip_address):
     url = "https://api.ip2location.io"
-    response = requests.get(url, params={
-        'ip': ip_address,
-        'key': GEOCODING_API_KEY
-    })
+    response = requests.get(url, params={"ip": ip_address, "key": GEOCODING_API_KEY})
 
     if response.status_code != 200:
         # Return empty dict if request failed
@@ -27,68 +43,90 @@ def geocode_ip_address(ip_address):
 
     # Extract the country and state from the response
     # This might change depending on the actual response structure
-    country = data.get('country_code', '')
-    state = data.get('region_name', '')
-    city = data.get('city_name', '')
+    country = data.get("country_code", "")
+    state = data.get("region_name", "")
+    city = data.get("city_name", "")
 
-    return {'country': country, 'state': state, 'city': city}
+    return {"country": country, "state": state, "city": city}
 
-spark = (SparkSession.builder
-         .getOrCreate())
-args = getResolvedOptions(sys.argv, ["JOB_NAME",
-                                     "ds",
-                                     'output_table',
-                                     'kafka_credentials',
-                                     'checkpoint_location'
-                                     ])
-run_date = args['ds']
-output_table = args['output_table']
-checkpoint_location = args['checkpoint_location']
-kafka_credentials = ast.literal_eval(args['kafka_credentials'])
+
+spark = SparkSession.builder.getOrCreate()
+args = getResolvedOptions(
+    sys.argv,
+    ["JOB_NAME", "ds", "output_table", "kafka_credentials", "checkpoint_location"],
+)
+run_date = args["ds"]
+output_table = args["output_table"]
+checkpoint_location = args["checkpoint_location"]
+kafka_credentials = ast.literal_eval(args["kafka_credentials"])
 glueContext = GlueContext(spark.sparkContext)
 spark = glueContext.spark_session
 
 # Retrieve Kafka credentials from environment variables
-kafka_key = kafka_credentials['KAFKA_WEB_TRAFFIC_KEY']
-kafka_secret = kafka_credentials['KAFKA_WEB_TRAFFIC_SECRET']
-kafka_bootstrap_servers = kafka_credentials['KAFKA_WEB_BOOTSTRAP_SERVER']
-kafka_topic = kafka_credentials['KAFKA_TOPIC']
+kafka_key = kafka_credentials["KAFKA_WEB_TRAFFIC_KEY"]
+kafka_secret = kafka_credentials["KAFKA_WEB_TRAFFIC_SECRET"]
+kafka_bootstrap_servers = kafka_credentials["KAFKA_WEB_BOOTSTRAP_SERVER"]
+kafka_topic = kafka_credentials["KAFKA_TOPIC"]
 
 if kafka_key is None or kafka_secret is None:
-    raise ValueError("KAFKA_WEB_TRAFFIC_KEY and KAFKA_WEB_TRAFFIC_SECRET must be set as environment variables.")
+    raise ValueError(
+        "KAFKA_WEB_TRAFFIC_KEY and KAFKA_WEB_TRAFFIC_SECRET must be set as environment variables."
+    )
 
 # Kafka configuration
 
 start_timestamp = f"{run_date}T00:00:00.000Z"
 
 # Define the schema of the Kafka message value
-schema = StructType([
-    StructField("user_id", StringType(), True),
-    StructField("url", StringType(), True),
-    StructField("referrer", StringType(), True),
-    StructField("user_agent", StructType([
-        StructField("family", StringType(), True),
-        StructField("major", StringType(), True),
-        StructField("minor", StringType(), True),
-        StructField("patch", StringType(), True),
-        StructField("device", StructType([
-            StructField("family", StringType(), True),
-            StructField("major", StringType(), True),
-            StructField("minor", StringType(), True),
-            StructField("patch", StringType(), True),
-        ]), True),
-        StructField("os", StructType([
-            StructField("family", StringType(), True),
-            StructField("major", StringType(), True),
-            StructField("minor", StringType(), True),
-            StructField("patch", StringType(), True),
-        ]), True)
-    ]), True),
-    StructField("headers", MapType(keyType=StringType(), valueType=StringType()), True),
-    StructField("host", StringType(), True),
-    StructField("ip", StringType(), True),
-    StructField("event_time", TimestampType(), True)
-])
+schema = StructType(
+    [
+        StructField("user_id", StringType(), True),
+        StructField("url", StringType(), True),
+        StructField("referrer", StringType(), True),
+        StructField(
+            "user_agent",
+            StructType(
+                [
+                    StructField("family", StringType(), True),
+                    StructField("major", StringType(), True),
+                    StructField("minor", StringType(), True),
+                    StructField("patch", StringType(), True),
+                    StructField(
+                        "device",
+                        StructType(
+                            [
+                                StructField("family", StringType(), True),
+                                StructField("major", StringType(), True),
+                                StructField("minor", StringType(), True),
+                                StructField("patch", StringType(), True),
+                            ]
+                        ),
+                        True,
+                    ),
+                    StructField(
+                        "os",
+                        StructType(
+                            [
+                                StructField("family", StringType(), True),
+                                StructField("major", StringType(), True),
+                                StructField("minor", StringType(), True),
+                                StructField("patch", StringType(), True),
+                            ]
+                        ),
+                        True,
+                    ),
+                ]
+            ),
+            True,
+        ),
+        StructField(
+            "headers", MapType(keyType=StringType(), valueType=StringType()), True
+        ),
+        StructField("host", StringType(), True),
+        StructField("ip", StringType(), True),
+        StructField("event_time", TimestampType(), True),
+    ]
+)
 
 
 spark.sql(f"""
@@ -110,81 +148,88 @@ PARTITIONED BY (session_date)
 """)
 
 # Read from Kafka in batch mode
-kafka_df = (spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
-    .option("subscribe", kafka_topic) \
-    .option("startingOffsets", "latest") \
-    .option("maxOffsetsPerTrigger", 10000) \
-    .option("kafka.security.protocol", "SASL_SSL") \
-    .option("kafka.sasl.mechanism", "PLAIN") \
-    .option("kafka.sasl.jaas.config",
-            f'org.apache.kafka.common.security.plain.PlainLoginModule required username="{kafka_key}" password="{kafka_secret}";') \
+kafka_df = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", kafka_bootstrap_servers)
+    .option("subscribe", kafka_topic)
+    .option("startingOffsets", "latest")
+    .option("maxOffsetsPerTrigger", 10000)
+    .option("kafka.security.protocol", "SASL_SSL")
+    .option("kafka.sasl.mechanism", "PLAIN")
+    .option(
+        "kafka.sasl.jaas.config",
+        f'org.apache.kafka.common.security.plain.PlainLoginModule required username="{kafka_key}" password="{kafka_secret}";',
+    )
     .load()
 )
 
+
 def decode_col(column):
-    return column.decode('utf-8')
+    return column.decode("utf-8")
+
 
 decode_udf = udf(decode_col, StringType())
 
-geocode_schema = StructType([
-    StructField("country", StringType(), True),
-    StructField("city", StringType(), True),
-    StructField("state", StringType(), True),
-])
+geocode_schema = StructType(
+    [
+        StructField("country", StringType(), True),
+        StructField("city", StringType(), True),
+        StructField("state", StringType(), True),
+    ]
+)
 
 geocode_udf = udf(geocode_ip_address, geocode_schema)
 
-session_window_df = kafka_df \
-    .withColumn("decoded_value", decode_udf(col("value"))) \
-    .withColumn("value", from_json(col("decoded_value"), schema)) \
-    .withColumn("geodata", geocode_udf(col("value.ip"))) \
+session_window_df = (
+    kafka_df.withColumn("decoded_value", decode_udf(col("value")))
+    .withColumn("value", from_json(col("decoded_value"), schema))
+    .withColumn("geodata", geocode_udf(col("value.ip")))
     .withWatermark("timestamp", "30 seconds")
+)
 
 
-group_by_df = session_window_df.groupBy(session_window(col("timestamp"), "5 minute"), # session window with 5 mins gap
-                                        col("value.user_id"),
-                                        col("value.ip"),
-                                        col("value.user_agent.os.family").alias("os"),
-                                        col("value.user_agent.family").alias("browser"),
-                                        col("geodata.country"),
-                                        col("geodata.city"),
-                                        col("geodata.state")
-                                        ) \
-    .count() \
-    .withColumn('is_user_logged',when( col('user_id').isNull() , False).otherwise(True) )\
-    .withColumn('session_id', sha2(concat_ws('_', col('user_id'),col('ip'),col('session_window.start')), 256)) \
-    .withColumn("session_date", date_format(col("session_window.start"), "yyyyMMdd").cast("int")) \
+group_by_df = (
+    session_window_df.groupBy(
+        session_window(col("timestamp"), "2 minute"),  # session window with 5 mins gap
+        col("value.user_id"),
+        col("value.ip"),
+        col("value.user_agent.os.family").alias("os"),
+        col("value.user_agent.family").alias("browser"),
+        col("geodata.country"),
+        col("geodata.city"),
+        col("geodata.state"),
+    )
+    .count()
     .select(
-        col("session_id"),
+        sha2(
+            concat_ws("_", col("user_id"), col("ip"), col("session_window.start")), 256
+        ).alias("session_id"),
         col("session_window.start").alias("session_window_start"),
         col("session_window.end").alias("session_window_end"),
-        col('session_date'),
+        date_format(col("session_window.start"), "yyyyMMdd")
+        .cast("int")
+        .alias("session_date"),
         col("country"),
         col("state"),
         col("city"),
         col("os"),
         col("browser"),
-        col("is_user_logged"),
-        col("count").alias("event_count")
-    ) 
+        when(col("user_id").isNull(), False).otherwise(True).alias("is_user_logged"),
+        col("count").alias("event_count"),
+    )
+)
 
-
-query = group_by_df \
-    .writeStream \
-    .format("iceberg") \
-    .outputMode("append") \
-    .trigger(processingTime="5 seconds") \
-    .option("fanout-enabled", "true") \
-    .option("checkpointLocation", checkpoint_location) \
+query = (
+    group_by_df.writeStream.format("iceberg")
+    .outputMode("append")
+    .trigger(processingTime="5 seconds")
+    .option("fanout-enabled", "true")
+    .option("checkpointLocation", checkpoint_location)
     .toTable(output_table)
+)
 
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # stop the job after 1 hour so we can capture real sessions
-query.awaitTermination(timeout=60*1*60)
-
-
+query.awaitTermination(timeout=60 * 60 * 1)
