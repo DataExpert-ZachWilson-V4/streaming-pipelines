9839aea31ec97f0e43a12e0da316f85199fd681d
AliaHa3 <eng.alia.hamwi@gmail.com>
2024-06-15 06:27:24 +0300

    upload script draft

--- submission/session_ddl.sql
+++ submission/session_ddl.sql
@@ -1,14 +1,16 @@
+-- Create a partitioned table for session data
 CREATE TABLE IF NOT EXISTS alia.tmp_sessions (
-  session_id STRING,
-  session_window_start STRING,
-  session_window_end STRING,
-  session_window_start_date STRING,
-  country STRING,
-  state STRING,
-  city STRING,
-  os STRING,
-  browser STRING,
-  is_user_logged Boolean,
-  event_count BIGINT
+session_id STRING,
+session_window_start TIMESTAMP ,
+session_window_end TIMESTAMP ,
+session_date INT,
+country STRING,
+state STRING,
+city STRING,
+os STRING,
+browser STRING,
+is_user_logged BOOLEAN,
+event_count BIGINT
 )
 USING ICEBERG
+PARTITIONED BY (session_date)
